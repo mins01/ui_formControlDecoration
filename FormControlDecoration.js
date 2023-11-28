6829d8c7681cc47105713f8ce3b772c87c6f62e7
mins01 <mins01.lycos.co.kr@gmail.com>
2023-11-28 17:53:54 +0900

    제작중

---
+++ FormControlDecoration.js
@@ -0,0 +1,43 @@
+class FormControlDecoration{
+    static instance = null;
+    target = null;
+    wrap = null;
+    static getInstance(){
+        if(!this.instance){
+            this.instance = new this();
+        }
+        return this.instance;
+    }
+    static activate(){
+        if(!globalThis.document){ console.log('globalThis.document is not exists.'); return;}
+        const instance = this.getInstance();
+        instance.addEventListener(globalThis.document)
+        instance.initialize();
+    }
+    static deactivate(){
+        this.getInstance().removeEventListener()
+    }
+    static sync(target){
+        this.getInstance().setData(target);
+    }
+
+
+
+
+    constructor(target){
+        this.target = target;
+        this.wrap = this.target.closest('.fcd-wrap');
+        this.decos = this.wrap.querySelectorAll('.fcd-deco');
+    }
+    camelToDash(camel){
+        return camel.replace(/[A-Z]/g, (c) => {"-" + c.toLowerCase()});
+    }
+    sync(){
+        this.decos.forEach((el) => {
+            el.innerText = this.target.getAttribute(el.dataset.fcdInnertext)
+        });
+    }
+    
+    
+
+}
